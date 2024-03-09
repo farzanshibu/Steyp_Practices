5e6b3d2fb50118607bd01c49ebc32ceea043cb5e
Farzan Shibu <farzanshibu786@gmail.com>
2024-03-09 23:40:34 +0530

    assessment-02

---
+++ js/assessment-02/month.js
@@ -0,0 +1,44 @@
+var d = new Date();
+
+let currentMonth = d.getMonth();
+
+switch (currentMonth) {
+  case 0:
+    console.log("January");
+    break;
+  case 1:
+    console.log("February");
+    break;
+  case 2:
+    console.log("March");
+    break;
+  case 3:
+    console.log("April");
+    break;
+  case 4:
+    console.log("May");
+    break;
+  case 5:
+    console.log("June");
+    break;
+  case 6:
+    console.log("July");
+    break;
+  case 7:
+    console.log("Augest");
+    break;
+  case 8:
+    console.log("September");
+    break;
+  case 9:
+    console.log("October");
+    break;
+  case 10:
+    console.log("November");
+    break;
+  case 11:
+    console.log("December");
+    break;
+  default:
+    console.log("Error Wrong Number");
+}
